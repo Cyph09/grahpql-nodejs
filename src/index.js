d671e184d80b60b0e17e613f8fcb1918c621bc4b
Swaleh Mhina <mhinasw@gmail.com>
2020-09-29 17:05:30 +0300

    Add post mutation & schema refactoring

--- src/index.js
+++ src/index.js
@@ -6,30 +6,30 @@ let links = [{
     description: 'Fullstack tutorial for GraphQL'
 }]
 
-const typeDefs = `
-type Query {
-    info: String!
-    feed: [Link!]!
-}
-
-type Link {
-    id: ID!
-    description: String!
-    url: String!
-}
-`
-
+let idCount = links.length
 const resolvers = {
     Query: {
         info: () => `This is the API of a Hackernews Clone`,
         feed: () => links
     },
 
+    Mutation: {
+        post: (parent, args) => {
+            const link = {
+                id: `link-${idCount++}`,
+                description: args.description,
+                url: args.url
+            }
+            links.push(link)
+            return link
+        }
+    }
+
    
 }
 
 const server = new GraphQLServer ({
-    typeDefs,
+    typeDefs: './src/schema.graphql',
     resolvers
 })
 server.start(() => console.log(`Server is running on port 4000`))
